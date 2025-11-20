963d6135263a29dbcb74f04363afb8498d59728d
kirylnikifarau <kiryl.nikifarau@innowise.com>
2025-11-20 14:45:21 +0300

    Init

---
+++ StrictFireAndForget/FireAndForgetClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace StrictFireAndForget
+{
+    public class FireAndForgetClass
+    {
+        public async void FireAndForgetMethod()
+        {
+            Console.WriteLine("Method started");
+            await Task.Delay(5000);
+            Console.WriteLine("Method finished");
+        }
+    }
+}
---
+++ StrictFireAndForget/Program.cs
@@ -0,0 +1,15 @@
+﻿using StrictFireAndForget;
+using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var fireAndForget = new FireAndForgetClass();
+
+        Console.WriteLine("Main started");
+        fireAndForget.FireAndForgetMethod();
+        Console.WriteLine("Main finished");
+    }
+}
